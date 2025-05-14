8324547238a13c591eb71d5dcd315bdd501c80ec
JZ <joshuazheng215@icloud.com>
2025-05-14 17:35:10 +1000

    Update CSS and JS files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './styles/global.css';
-import HelpPage from './components/HelpPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/help" element={<HelpPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
